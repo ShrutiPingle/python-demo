5cb54303541c2429a2cff780d1adfb94c51b19f3
ShrutiPingle <shruti.pingle04@gmail.com>
2026-09-10 13:52:25 +0530

    hehehe

---
+++ firts.py
@@ -0,0 +1,23 @@
+# a= int(input('enter a number to check if it is even '))
+# c= a%2!=0
+# print(f"number is odd : {c}")
+
+# age= int(input('enter your age to check how many days you have been on earth '))
+# print(f"{age} years = {age*365} days")
+
+# min= int(input('enter minutes to check in hours '))
+# print(f"{min} is {min//60} hours {min%60} minutes")
+
+# num= int(input('enter number for its last digit '))
+# print(f"{num} : last digit is {num%10}")
+
+# role = input('enter role ')
+# age2 = int(input('enter age '))
+# print(f"Eligible : {'student'==role and age2 >=21}")
+
+# swap
+# a=10
+# b=20
+# print(f"a={a} and b={b}")
+# b=a=b
+# print(f"a={a} and b={b}")
